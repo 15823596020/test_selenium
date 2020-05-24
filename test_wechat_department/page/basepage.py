b60443812242ebf70e8c140fb852e99c9364a88c
15823596020 <519312875@qq.com>
2020-05-24 17:57:52 +0800

    wechat_department

--- test_wechat_department/page/basepage.py
+++ test_wechat_department/page/basepage.py
@@ -11,12 +11,11 @@ class BasePage:
             option = Options()  # 实例化Options类
             option.debugger_address = "localhost:9222"  # 调用debugger_address方法，并传入调试地址为本地启动的一个服务
             self.driver = webdriver.Chrome(options=option)  # 实例化
+            self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")  # 打开企业微信的通讯录页面
+            self.driver.maximize_window()  # 窗口最大化
         else:
             # 否则self.driver = driver1,self.driver后面的: WebDriver是给self.driver加的类型提示，用于后面好调用该类型的方法，没有任何意义
             self.driver: WebDriver = driver1
-
-        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")  # 打开企业微信的通讯录页面
-        self.driver.maximize_window()  # 窗口最大化
         self.driver.implicitly_wait(3)  # 隐式等待
 
     # 封装self.driver.find_element，提供find方法，并返回；这里需传入连个参数，by-查找的方式，value-具体定位的元素信息
